8835e35764c5f1ba7e608383a86a9eef4a61e629
Owen Rogers <orogers@wesleyan.edu>
2025-04-22 15:36:08 -0400

    Update plotting_misc.py

--- plotting_misc.py
+++ plotting_misc.py
@@ -8,8 +8,66 @@ from matplotlib.colors import LightSource
 
 
 """
-This is where I'll store all of the functions and classes for making figures for my thesis.
+This is where I stored code for making the free energy landscape plot from my thesis. A really fun script to make, but overall not very useful. 
 """
+
+
+class EnergySurface:
+    def __init__(self, lb, ub, step):
+
+        self.lb = int(lb)
+        self.ub = int(ub)
+        self.step = int(step)
+
+        self.x = np.linspace(self.lb, self.ub, self.step)
+        self.y = np.linspace(self.lb, self.ub, self.step)
+
+        self.X, self.Y = np.meshgrid(self.x, self.y)
+        self.Z = np.zeros_like(self.X)
+
+        self.features = []
+
+    def add_feature(self, feature):
+        if isinstance(feature, (Peak, Trough)):
+            self.features.append(feature)
+            self.Z += feature.add_to_Z_array(self.X, self.Y)
+        else:
+            raise ValueError('Feature must be Peak or Trough')
+
+    def reset(self):
+        self.Z = np.zeros_like(self.X)
+        self.features = []
+
+    def X_array(self):
+        return self.X
+
+    def Y_array(self):
+        return self.Y
+
+    def Z_array(self):
+        return self.Z
+
+
+class Peak:
+    def __init__(self, height=1, width=0.1, center_x=0, center_y=0):
+        self.height = height
+        self.width = width
+        self.center_x = center_x
+        self.center_y = center_y
+
+    def add_to_Z_array(self, X, Y):
+        return self.height * np.exp(-((X - self.center_x) ** 2 + (Y - self.center_y) ** 2) / (2 * self.width ** 2))
+
+
+class Trough:
+    def __init__(self, height=1, width=0.1, center_x=0, center_y=0):
+        self.height = height
+        self.width = width
+        self.center_x = center_x
+        self.center_y = center_y
+
+    def add_to_Z_array(self, X, Y):
+        return -self.height * np.exp(-((X - self.center_x) ** 2 + (Y - self.center_y) ** 2) / (2 * self.width ** 2))
 
 
 def make_color_list(vals_list, color1=(245, 66, 129), color2=(92, 244, 255)):
@@ -76,63 +134,3 @@ def plot_surface(surface_obj, azimuth, altitude, save=False, dpi=100, colormap='
         os.makedirs('./surfaces', exist_ok=True)
 
         figure.savefig(f'surfaces/surface_{azimuth}_{altitude}.png', dpi=dpi)
-
-
-class EnergySurface:
-    def __init__(self, lb, ub, step):
-
-        self.lb = int(lb)
-        self.ub = int(ub)
-        self.step = int(step)
-
-        self.x = np.linspace(self.lb, self.ub, self.step)
-        self.y = np.linspace(self.lb, self.ub, self.step)
-
-        self.X, self.Y = np.meshgrid(self.x, self.y)
-        self.Z = np.zeros_like(self.X)
-
-        self.features = []
-
-    def add_feature(self, feature):
-        if isinstance(feature, (Peak, Trough)):
-            self.features.append(feature)
-            self.Z += feature.add_to_Z_array(self.X, self.Y)
-        else:
-            raise ValueError('Feature must be Peak or Trough')
-
-    def reset(self):
-        self.Z = np.zeros_like(self.X)
-        self.features = []
-
-    def X_array(self):
-        return self.X
-
-    def Y_array(self):
-        return self.Y
-
-    def Z_array(self):
-        return self.Z
-
-
-class Peak:
-    def __init__(self, height=1, width=0.1, center_x=0, center_y=0):
-        self.height = height
-        self.width = width
-        self.center_x = center_x
-        self.center_y = center_y
-
-    def add_to_Z_array(self, X, Y):
-        return self.height * np.exp(-((X - self.center_x) ** 2 + (Y - self.center_y) ** 2) / (2 * self.width ** 2))
-
-
-class Trough:
-    def __init__(self, height=1, width=0.1, center_x=0, center_y=0):
-        self.height = height
-        self.width = width
-        self.center_x = center_x
-        self.center_y = center_y
-
-    def add_to_Z_array(self, X, Y):
-        return -self.height * np.exp(-((X - self.center_x) ** 2 + (Y - self.center_y) ** 2) / (2 * self.width ** 2))
-
-
